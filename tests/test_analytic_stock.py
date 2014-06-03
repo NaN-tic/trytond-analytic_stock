e64bd167b85c36a63945e83657ffcc2788f0a7ff
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-06-03 09:37:27 +0200

    Cleanup tests

--- tests/test_analytic_stock.py
+++ tests/test_analytic_stock.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import datetime
 from decimal import Decimal
@@ -286,6 +278,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AnalyticStockTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
